26b2da08cdc988be02ad18c766760be22db97d92
leebean <74585673+delight-HK3@users.noreply.github.com>
2022-02-22 06:30:51 +0900

    Update L_main.php
    코드 정리 작업을 했습니다.

--- controller/L_main.php
+++ controller/L_main.php
@@ -1,14 +1,14 @@
-<?
-class L_main extends CI_Controller {               // 클래스이름 첫 글자는 대문자
+<?php
+class L_main extends CI_Controller { // 클래스이름 첫 글자는 대문자
 	function __construct(){
-        parent::__construct();
-        $this->load->database();                     // 데이터베이스 연결
-        $this->load->model("l_main_m");    // 모델 l_main_m 연결
+        	parent::__construct();
+        	$this->load->database(); // 데이터베이스 연결
+        	$this->load->model("l_main_m");    // 모델 l_main_m 연결
 		$this->load->helper(array("url", "date"));   
 		$this->load->library("pagination");//라이브러리 선언
-    }
-    public function index()                              // 제일 먼저 실행되는 함수
-    {   
+    	}
+    	public function index()                      
+    	{   
 		$uri_array=$this->uri->uri_to_assoc(3);
 		$text1 = array_key_exists("text1",$uri_array) ? urldecode($uri_array["text1"]) : "" ;
 		$data["text1"]=$text1;						// text1 값 전달을 위한 처리
@@ -16,18 +16,18 @@ class L_main extends CI_Controller {               // 클래스이름 첫 글자
 		$data["list1"]=$this->l_main_m->getgubun();   // 해당페이지 자료읽기
 		$data["list2"]=$this->l_main_m->getbrand();   // 해당페이지 자료읽기
 
-        $this->load->view("l_main_header",$data);                    // 상단출력(메뉴)
+        	$this->load->view("l_main_header",$data); // 상단출력(메뉴)
 		$this->load->view("l_introduce");
-        $this->load->view("l_main_footer"); 
-    }
-	public function find(){
+        	$this->load->view("l_main_footer"); 
+	}
+	public function find()
+	{
 		$uri_array=$this->uri->uri_to_assoc(3);
 		$text1 = array_key_exists("text1",$uri_array) ? urldecode($uri_array["text1"]) : "" ;
 			
 		$base_url = "/l_main/find/text1/$text1/page";
 		
 		$page_segment = substr_count( substr($base_url,0,strpos($base_url,"page")) , "/" )+1;
-		$base_url = "/~sale25" . $base_url;
 
 		$config["per_page"] = 8;                              // 페이지당 표시할 line 수
 		$config["total_rows"] = $this->l_main_m->rowcount($text1);  // 전체 레코드개수 구하기
@@ -48,9 +48,9 @@ class L_main extends CI_Controller {               // 클래스이름 첫 글자
 		$maindata["list1"]=$this->l_main_m->getgubun();   // 해당페이지 자료읽기
 		$maindata["list2"]=$this->l_main_m->getbrand();   // 해당페이지 자료읽기
 
-        $this->load->view("l_main_header",$maindata);                    // 상단출력(메뉴)
-        $this->load->view("l_findproduct_list",$data);           // a_member_list에 자료전달
-        $this->load->view("l_main_footer");                      // 하단 출력 
+        	$this->load->view("l_main_header",$maindata);                    // 상단출력(메뉴)
+        	$this->load->view("l_findproduct_list",$data);           // a_member_list에 자료전달
+        	$this->load->view("l_main_footer");                      // 하단 출력 
 	}
 }
 ?>
