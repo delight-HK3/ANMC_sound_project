5e79305d5cca8ad0264f2d68c16ccd51135d95cd
leebean <74585673+delight-HK3@users.noreply.github.com>
2022-02-22 06:07:31 +0900

    Update A_gubun.php
    코드 정리 작업을 했습니다.

--- controller/A_gubun.php
+++ controller/A_gubun.php
@@ -1,105 +1,105 @@
-<?
-    class A_gubun extends CI_Controller {       // gubun클래스 선언
-        function __construct(){
-            parent::__construct();
-            $this->load->database();                     // 데이터베이스 연결
-            $this->load->model("a_gubun_m");    // 모델 gubun_m 연결
-			$this->load->helper(array("url", "date"));   
-			$this->load->library('pagination');
+<?php
+class A_gubun extends CI_Controller {       // gubun클래스 선언
+	function __construct(){
+            	parent::__construct();
+            	$this->load->database(); // 데이터베이스 연결
+            	$this->load->model("a_gubun_m");    // 모델 gubun_m 연결
+		$this->load->helper(array("url", "date"));   
+		$this->load->library('pagination');
         }
         public function index()// 제일 먼저 실행되는 함수
-		{
-            $this->lists();                                        // list 함수 호출
+	{
+            	$this->lists(); // list 함수 호출
         }
-        public function lists(){
-			$uri_array=$this->uri->uri_to_assoc(3);
-			$text1 = array_key_exists("text1",$uri_array) ? urldecode($uri_array["text1"]) : "" ;
+        public function lists()
+	{
+		$uri_array=$this->uri->uri_to_assoc(3);
+		$text1 = array_key_exists("text1",$uri_array) ? urldecode($uri_array["text1"]) : "" ;
 			
-			if ($text1==""){ 
-				$base_url = "/a_gubun/lists/page"; 
-			}
-			else{ 
-				$base_url = "/a_gubun/lists/text1/$text1/page";
-			}
-			$page_segment = substr_count( substr($base_url,0,strpos($base_url,"page")) , "/" )+1;
-			$base_url = "/~sale25" . $base_url;
+		if ($text1==""){ 
+			$base_url = "/a_gubun/lists/page"; 
+		}
+		else{ 
+			$base_url = "/a_gubun/lists/text1/$text1/page";
+		}
+		$page_segment = substr_count( substr($base_url,0,strpos($base_url,"page")) , "/" )+1;
+		$base_url = "/~sale25" . $base_url;
 
-			$config["per_page"] = 5;                              // 페이지당 표시할 line 수
-			$config["total_rows"] = $this->a_gubun_m->rowcount($text1);  // 전체 레코드개수 구하기
-			$config["uri_segment"] = $page_segment;		 // 페이지가 있는 segment 위치
-			$config["base_url"] = $base_url;                // 기본 URL
-			$this->pagination->initialize($config);           // pagination 설정 적용
+		$config["per_page"] = 5;                              // 페이지당 표시할 line 수
+		$config["total_rows"] = $this->a_gubun_m->rowcount($text1);  // 전체 레코드개수 구하기
+		$config["uri_segment"] = $page_segment;		 // 페이지가 있는 segment 위치
+		$config["base_url"] = $base_url;                // 기본 URL
+		$this->pagination->initialize($config);           // pagination 설정 적용
 			
-			$data["page"]=$this->uri->segment($page_segment,0);  // 시작위치, 없으면 0.
-			$data["pagination"] = $this->pagination->create_links();  // 페이지소스 생성
+		$data["page"]=$this->uri->segment($page_segment,0);  // 시작위치, 없으면 0.
+		$data["pagination"] = $this->pagination->create_links();  // 페이지소스 생성
 			
-			$start=$data["page"];                 // n페이지 : 시작위치
-			$limit=$config["per_page"];        // 페이지 당 라인수
+		$start=$data["page"];                 // n페이지 : 시작위치
+		$limit=$config["per_page"];        // 페이지 당 라인수
 
-			$data["text1"]=$text1;						// text1 값 전달을 위한 처리
-			$data["list"]=$this->a_gubun_m->getlist($text1,$start,$limit);   // 해당페이지 자료읽기
+		$data["text1"]=$text1;						// text1 값 전달을 위한 처리
+		$data["list"]=$this->a_gubun_m->getlist($text1,$start,$limit);   // 해당페이지 자료읽기
 
-            $this->load->view("a_main_header");                    // 상단출력(메뉴)
-            $this->load->view("a_gubun_list",$data);           // gubun_list에 자료전달
-            $this->load->view("a_main_footer");                      // 하단 출력 
+            	$this->load->view("a_main_header");                    // 상단출력(메뉴)
+            	$this->load->view("a_gubun_list",$data);           // gubun_list에 자료전달
+            	$this->load->view("a_main_footer");                      // 하단 출력 
         }
-		public function noajax_lists(){
-			$uri_array=$this->uri->uri_to_assoc(3);
-			$text1 = array_key_exists("text1",$uri_array) ? urldecode($uri_array["text1"]) : "" ;
+	public function noajax_lists()
+	{
+		$uri_array=$this->uri->uri_to_assoc(3);
+		$text1 = array_key_exists("text1",$uri_array) ? urldecode($uri_array["text1"]) : "" ;
 			
-			if ($text1==""){ 
-				$base_url = "/a_gubun/noajax_lists/page"; 
-			}
-			else{ 
-				$base_url = "/a_gubun/noajax_lists/text1/$text1/page";
-			}
-			$page_segment = substr_count( substr($base_url,0,strpos($base_url,"page")) , "/" )+1;
-			$base_url = "/~sale25" . $base_url;
+		if ($text1==""){ 
+			$base_url = "/a_gubun/noajax_lists/page"; 
+		}
+		else{ 
+			$base_url = "/a_gubun/noajax_lists/text1/$text1/page";
+		}
+		$page_segment = substr_count( substr($base_url,0,strpos($base_url,"page")) , "/" )+1;
 
-			$config["per_page"] = 5;                              // 페이지당 표시할 line 수
-			$config["total_rows"] = $this->a_gubun_m->rowcount($text1);  // 전체 레코드개수 구하기
-			$config["uri_segment"] = $page_segment;		 // 페이지가 있는 segment 위치
-			$config["base_url"] = $base_url;                // 기본 URL
-			$this->pagination->initialize($config);           // pagination 설정 적용
+		$config["per_page"] = 5;                              // 페이지당 표시할 line 수
+		$config["total_rows"] = $this->a_gubun_m->rowcount($text1);  // 전체 레코드개수 구하기
+		$config["uri_segment"] = $page_segment;		 // 페이지가 있는 segment 위치
+		$config["base_url"] = $base_url;                // 기본 URL
+		$this->pagination->initialize($config);           // pagination 설정 적용
 			
-			$data["page"]=$this->uri->segment($page_segment,0);  // 시작위치, 없으면 0.
-			$data["pagination"] = $this->pagination->create_links();  // 페이지소스 생성
+		$data["page"]=$this->uri->segment($page_segment,0);  // 시작위치, 없으면 0.
+		$data["pagination"] = $this->pagination->create_links();  // 페이지소스 생성
 			
-			$start=$data["page"];                 // n페이지 : 시작위치
-			$limit=$config["per_page"];        // 페이지 당 라인수
+		$start=$data["page"];                 // n페이지 : 시작위치
+		$limit=$config["per_page"];        // 페이지 당 라인수
 
-			$data["text1"]=$text1;						// text1 값 전달을 위한 처리
-			$data["list"]=$this->a_gubun_m->getlist($text1,$start,$limit);   // 해당페이지 자료읽기
+		$data["text1"]=$text1;						// text1 값 전달을 위한 처리
+		$data["list"]=$this->a_gubun_m->getlist($text1,$start,$limit);   // 해당페이지 자료읽기
 
-            $this->load->view("a_main_header_nomenu");                    // 상단출력(메뉴)
-            $this->load->view("a_findgubun_list",$data);           // gubun_list에 자료전달
-            $this->load->view("a_main_footer");                      // 하단 출력 
+            	$this->load->view("a_main_header_nomenu");                    // 상단출력(메뉴)
+            	$this->load->view("a_findgubun_list",$data);           // gubun_list에 자료전달
+            	$this->load->view("a_main_footer");                      // 하단 출력 
         }
-		public function a_gubun_insert()
+	public function a_gubun_insert()
         {
-            $name=$this->input->post("name",TRUE);
-            $data = array(
-                "name25"=>$name
-            );
-            $this->a_gubun_m->insertrow($data);
-            $no=$this->db->insert_id();
-            if($no) echo $no; // 내부적으로 호출
+            	$name=$this->input->post("name",TRUE);
+            	$data = array(
+                	"name25"=>$name
+            	);
+            	$this->a_gubun_m->insertrow($data);
+            	$no=$this->db->insert_id();
+            	if($no) echo $no; // 내부적으로 호출
         }
         public function a_gubun_delete()
         {
-            $no = $this->input->post("no",TRUE);
-            $result = $this->a_gubun_m->deleterow($no);
-            if($result) echo $no; //있어도상관없고 없어도 상관없음
-        }
-
-		public function a_gubun_update()//사용자 수정
-		{
-			$name=$this->input->post("name",TRUE);
-			$data = array(	
-				'name25'=>$name
-			);
-			$no = $this->input->post("no",TRUE);
-			$this->a_gubun_m->updaterow($data,$no);
-		}
-    }
+            	$no = $this->input->post("no",TRUE);
+            	$result = $this->a_gubun_m->deleterow($no);
+            	if($result) echo $no; 
+	}
+	public function a_gubun_update()//사용자 수정
+	{
+		$name=$this->input->post("name",TRUE);
+		$data = array(	
+			'name25'=>$name
+		);
+		$no = $this->input->post("no",TRUE);
+		$this->a_gubun_m->updaterow($data,$no);
+	}
+}
 ?>
