f9944a90e809c9f0174d5647eb2c2d9a826969eb
leebean <74585673+delight-HK3@users.noreply.github.com>
2022-02-22 06:24:01 +0900

    Update L_brandproduct.php
    코드 정리 작업을 했습니다.

--- controller/L_brandproduct.php
+++ controller/L_brandproduct.php
@@ -1,67 +1,67 @@
-<?
-    class L_brandproduct extends CI_Controller {       // product클래스 선언
-        function __construct(){
-            parent::__construct();
-            $this->load->database();                     // 데이터베이스 연결
-            $this->load->model("l_brandproduct_m");    // 모델 product_m 연결
-			$this->load->model("l_main_m");    // 모델 l_main_m 연결
-			$this->load->helper(array("url", "date"));   
-			$this->load->library('pagination');
+<?php
+class L_brandproduct extends CI_Controller { // product클래스 선언
+	function __construct(){
+        	parent::__construct();
+            	$this->load->database(); // 데이터베이스 연결
+            	$this->load->model("l_brandproduct_m");    // 모델 product_m 연결
+		$this->load->model("l_main_m");    // 모델 l_main_m 연결
+		$this->load->helper(array("url", "date"));   
+		$this->load->library('pagination');
         }
-        public function lists(){
-			
-			$uri_array=$this->uri->uri_to_assoc(3);
-			$no	=  $this->uri->segment(4);
-			$text1 = array_key_exists("text1",$uri_array) ? urldecode($uri_array["text1"]) : "" ;
-			if ($text1==""){ 
-				$base_url = "/l_brandproduct/lists/no/$no/page"; 
-			}
-			else{ 
-				$base_url = "/l_brandproduct/lists/no/$no//text1/$text1/page";
-			}
-			$page_segment = substr_count( substr($base_url,0,strpos($base_url,"page")) , "/" )+1;
-			$base_url = "/~sale25" . $base_url;
-			
-			$config["per_page"] = 8;                              // 페이지당 표시할 line 수
-			$config["total_rows"] = $this->l_brandproduct_m->rowcount($no);  // 전체 레코드개수 구하기
-			$config["uri_segment"] = $page_segment;		 // 페이지가 있는 segment 위치
-			$config["base_url"] = $base_url;                // 기본 URL
-			$this->pagination->initialize($config);           // pagination 설정 적용
-			
-			$data["page"]=$this->uri->segment($page_segment,0);  // 시작위치, 없으면 0.
-			$data["pagination"] = $this->pagination->create_links();  // 페이지소스 생성
-			
-			$start=$data["page"];                 // n페이지 : 시작위치
-			$limit=$config["per_page"];        // 페이지 당 라인수
-
-			$data["list"]=$this->l_brandproduct_m->getlist($start,$limit,$no);   // 해당페이지 자료읽기
-			$data["brandname"]=$this->l_brandproduct_m->brandname($no);
-			$data["total"]= $this->l_brandproduct_m->rowcount($no);
-
-			$maindata["text1"]=$text1;						// text1 값 전달을 위한 처리
-			$maindata["list1"]=$this->l_main_m->getgubun();   // 해당페이지 자료읽기
-			$maindata["list2"]=$this->l_main_m->getbrand();   // 해당페이지 자료읽기
-			
-            $this->load->view("l_main_header",$maindata);                    // 상단출력(메뉴)
-            $this->load->view("l_brandproduct_list",$data);           // product_list에 자료전달
-            $this->load->view("l_main_footer");                      // 하단 출력 
-        }
-		public function view(){
-			$uri_array=$this->uri->uri_to_assoc(3);
-			$no	= array_key_exists("no",$uri_array) ? $uri_array["no"] : "" ;
-			$page = array_key_exists("page",$uri_array) ? urldecode($uri_array["page"]) : "" ;
-			$text1 = array_key_exists("text1",$uri_array) ? urldecode($uri_array["text1"]) : "" ;
-
-			$data["page"] = $page;
-			$data["row"] = $this->l_brandproduct_m->getrow($no);
-
-			$maindata["text1"]=$text1;						// text1 값 전달을 위한 처리
-			$maindata["list1"]=$this->l_main_m->getgubun();   // 해당페이지 자료읽기
-			$maindata["list2"]=$this->l_main_m->getbrand();   // 해당페이지 자료읽기	
-
-			$this->load->view("l_main_header",$maindata);                    // 상단출력(메뉴)
-            $this->load->view("l_brandproduct_view",$data);           // product_list에 자료전달
-            $this->load->view("l_main_footer");     
+        public function lists()
+	{
+		$uri_array=$this->uri->uri_to_assoc(3);
+		$no = $this->uri->segment(4);
+		$text1 = array_key_exists("text1",$uri_array) ? urldecode($uri_array["text1"]) : "" ;
+		if ($text1==""){ 
+			$base_url = "/l_brandproduct/lists/no/$no/page"; 
 		}
-    }
+		else{ 
+			$base_url = "/l_brandproduct/lists/no/$no//text1/$text1/page";
+		}
+		$page_segment = substr_count( substr($base_url,0,strpos($base_url,"page")) , "/" )+1;
+			
+		$config["per_page"] = 8;                              // 페이지당 표시할 line 수
+		$config["total_rows"] = $this->l_brandproduct_m->rowcount($no);  // 전체 레코드개수 구하기
+		$config["uri_segment"] = $page_segment;		 // 페이지가 있는 segment 위치
+		$config["base_url"] = $base_url;                // 기본 URL
+		$this->pagination->initialize($config);           // pagination 설정 적용
+			
+		$data["page"]=$this->uri->segment($page_segment,0);  // 시작위치, 없으면 0.
+		$data["pagination"] = $this->pagination->create_links();  // 페이지소스 생성
+			
+		$start=$data["page"];                 // n페이지 : 시작위치
+		$limit=$config["per_page"];        // 페이지 당 라인수
+
+		$data["list"]=$this->l_brandproduct_m->getlist($start,$limit,$no);   // 해당페이지 자료읽기
+		$data["brandname"]=$this->l_brandproduct_m->brandname($no);
+		$data["total"]= $this->l_brandproduct_m->rowcount($no);
+
+		$maindata["text1"]=$text1;						// text1 값 전달을 위한 처리
+		$maindata["list1"]=$this->l_main_m->getgubun();   // 해당페이지 자료읽기
+		$maindata["list2"]=$this->l_main_m->getbrand();   // 해당페이지 자료읽기
+			
+            	$this->load->view("l_main_header",$maindata); // 상단출력(메뉴)
+            	$this->load->view("l_brandproduct_list",$data); // product_list에 자료전달
+            	$this->load->view("l_main_footer"); // 하단 출력 
+        }
+	public function view()
+	{
+		$uri_array=$this->uri->uri_to_assoc(3);
+		$no = array_key_exists("no",$uri_array) ? $uri_array["no"] : "" ;
+		$page = array_key_exists("page",$uri_array) ? urldecode($uri_array["page"]) : "" ;
+		$text1 = array_key_exists("text1",$uri_array) ? urldecode($uri_array["text1"]) : "" ;
+
+		$data["page"] = $page;
+		$data["row"] = $this->l_brandproduct_m->getrow($no);
+
+		$maindata["text1"]=$text1; // text1 값 전달을 위한 처리
+		$maindata["list1"]=$this->l_main_m->getgubun();   // 해당페이지 자료읽기
+		$maindata["list2"]=$this->l_main_m->getbrand();   // 해당페이지 자료읽기	
+
+		$this->load->view("l_main_header",$maindata); // 상단출력(메뉴)
+            	$this->load->view("l_brandproduct_view",$data); // product_list에 자료전달
+            	$this->load->view("l_main_footer");     
+	}
+}
 ?>
