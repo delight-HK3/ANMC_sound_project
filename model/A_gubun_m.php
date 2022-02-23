e62b278fa17a23451e2ab7e69112a7014d495a48
leebean <74585673+delight-HK3@users.noreply.github.com>
2022-02-23 09:55:39 +0900

    Update A_gubun_m.php
    코드 정리 작업을 했습니다.

--- model/A_gubun_m.php
+++ model/A_gubun_m.php
@@ -1,41 +1,41 @@
-<?
-    class A_gubun_m extends CI_Model     // 모델 클래스 선언
-    {
+<?php
+class A_gubun_m extends CI_Model{     // 모델 클래스 선언
         public function getlist($text1,$start,$limit)//정보목록보기
         {
-            if (!$text1){
-				$sql="select * from progubun order by name25 limit $start,$limit";    
-			}
-			else{
-				$sql="select * from progubun where name25 like '%$text1%' order by name25 limit $start,$limit";
-			}
-			return $this->db->query($sql)->result();           // 쿼리실행, 결과 리턴
+		if (!$text1){
+			$sql="select * from progubun order by name25 limit $start,$limit";    
+		}
+		else{
+			$sql="select * from progubun where name25 like '%$text1%' order by name25 limit $start,$limit";
+		}
+		return $this->db->query($sql)->result();           // 쿼리실행, 결과 리턴
         }
-		public function rowcount( $text1 ){
-			if (!$text1){
-				$sql="select * from progubun order by name25";
-			}
-			else{
-				$sql="select * from progubun where name25 like '%$text1%' ";
-			}
-			return $this->db->query($sql)->num_rows();
+	public function rowcount($text1)
+	{
+		if (!$text1){
+			$sql="select * from progubun order by name25";
 		}
-		function getrow($no)  {//정보추가
-			$sql="select * from progubun where no25=$no";
-			return  $this->db->query($sql)->row();
+		else{
+			$sql="select * from progubun where name25 like '%$text1%' ";
 		}
-		function deleterow($no)  {//정보삭제
-			$sql="delete from progubun where no25=$no";
-			return $this->db->query($sql);
-		}
-		function insertrow($row)//정보추가
-		{
-			return $this->db->insert("progubun",$row);
-		}
-		function updaterow($row,$no)//정보수정
-		{
-			$where=array("no25"=>$no);
-		    return $this->db->update("progubun",$row,$where);
-		}
-    }
+		return $this->db->query($sql)->num_rows();
+	}
+	function getrow($no)  {//정보추가
+		$sql="select * from progubun where no25=$no";
+		return  $this->db->query($sql)->row();
+	}
+	function deleterow($no)  {//정보삭제
+		$sql="delete from progubun where no25=$no";
+		return $this->db->query($sql);
+	}
+	function insertrow($row)//정보추가
+	{
+		return $this->db->insert("progubun",$row);
+	}
+	function updaterow($row,$no)//정보수정
+	{
+		$where=array("no25"=>$no);
+		return $this->db->update("progubun",$row,$where);
+	}
+}
 ?>
