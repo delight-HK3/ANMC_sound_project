60a2adf7de8fa866e5030acdf56c8cdce5b553b2
leebean <74585673+delight-HK3@users.noreply.github.com>
2022-02-23 10:51:09 +0900

    Update L_main_m.php
    코드 정리 작업을 했습니다.

--- model/L_main_m.php
+++ model/L_main_m.php
@@ -1,35 +1,35 @@
-<?
-    class L_main_m extends CI_Model     // 모델 클래스 선언
-    {
-        public function getgubun()//정보목록보기
+<?php
+class L_main_m extends CI_Model{     // 모델 클래스 선언
+	public function getgubun()//정보목록보기
         {
-			$sql="select * from progubun";    
-			return $this->db->query($sql)->result();           // 쿼리실행, 결과 리턴
+		$sql="select * from progubun";    
+		return $this->db->query($sql)->result();           // 쿼리실행, 결과 리턴
         }
-		public function getbrand()//정보목록보기
+	public function getbrand()//정보목록보기
         {
-			$sql="select * from probrand";    
-			return $this->db->query($sql)->result();           // 쿼리실행, 결과 리턴
+		$sql="select * from probrand";    
+		return $this->db->query($sql)->result();           // 쿼리실행, 결과 리턴
         }
-		 public function getlist($text1,$start,$limit)//정보목록보기
+	public function getlist($text1,$start,$limit)//정보목록보기
         {
-			$sql="select proproduct.*, 
-						progubun.name25 as gubun_name25,
-						probrand.name25 as brand_name25
-						from proproduct 
-						left join progubun on 
-						proproduct.progubun_no25=progubun.no25 
-						left join probrand on
-						proproduct.probrand_no25=probrand.no25 where
-						proproduct.name25 like '%$text1%'
-						order by proproduct.price25 DESC limit $start,$limit";
-			return $this->db->query($sql)->result();// 쿼리실행, 결과 리턴
+		$sql="select proproduct.*, 
+			progubun.name25 as gubun_name25,
+			probrand.name25 as brand_name25
+			from proproduct 
+			left join progubun on 
+			proproduct.progubun_no25=progubun.no25 
+			left join probrand on
+			proproduct.probrand_no25=probrand.no25 where
+			proproduct.name25 like '%$text1%'
+			order by proproduct.price25 DESC limit $start,$limit";
+		
+		return $this->db->query($sql)->result();// 쿼리실행, 결과 리턴
         }
-		public function rowcount( $text1 )
-			{
-			$sql="select * from proproduct where name25 like '%$text1%'";
-			return $this->db->query($sql)->num_rows();
-		}
-    }
+	public function rowcount($text1)
+	{
+		$sql="select * from proproduct where name25 like '%$text1%'";
+		return $this->db->query($sql)->num_rows();
+	}
+}
 ?>
 
