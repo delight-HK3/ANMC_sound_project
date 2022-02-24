8d4ca13fa896609c6a2f4bbc72eccd4f798e2b99
leebean <74585673+delight-HK3@users.noreply.github.com>
2022-02-24 11:31:59 +0900

    Update a_main_header.php
    script, a태그의 링크 수정 했고 코드 정리 작업을 했습니다.

--- view/a_main_header.php
+++ view/a_main_header.php
@@ -1,45 +1,45 @@
 <!DOCTYPE html>
 <html lang="kr">
 	<head>
-	<meta charset="utf-8">
-	<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-	<title>개별프로젝트 관리자 페이지</title>
-		<link href="/~sale25/my/css/bootstrap.css" rel="stylesheet">
-		<link href="/~sale25/my/css/my.css" rel="stylesheet">
-		<link href="/~sale25/my/css/bootstrap-datetimepicker.css" rel="stylesheet">
-		<link href="/~sale25/my/css/fontawesome-all.css" rel="stylesheet">
-		<link href="/~sale25/my/css/ihover.css" rel="stylesheet">
-		<link href="/~sale25/my/css/ihover.min.css" rel="stylesheet">		
+		<meta charset="utf-8">
+		<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+		<title>개별프로젝트 관리자 페이지</title>
+		<link href="/my/css/bootstrap.css" rel="stylesheet">
+		<link href="/my/css/my.css" rel="stylesheet">
+		<link href="/my/css/bootstrap-datetimepicker.css" rel="stylesheet">
+		<link href="/my/css/fontawesome-all.css" rel="stylesheet">
+		<link href="/my/css/ihover.css" rel="stylesheet">
+		<link href="/my/css/ihover.min.css" rel="stylesheet">		
 
-		<script src="/~sale25/my/js/jquery-3.5.1.min.js"></script>
-		<script src="/~sale25/my/js/popper.min.js"></script>
-		<script src="/~sale25/my/js/bootstrap.min.js"></script>
-		<script src="/~sale25/my/js/moment-with-locales.min.js"></script>
-		<script src="/~sale25/my/js/bootstrap-datetimepicker.js"></script>
+		<script src="/my/js/jquery-3.5.1.min.js"></script>
+		<script src="/my/js/popper.min.js"></script>
+		<script src="/my/js/bootstrap.min.js"></script>
+		<script src="/my/js/moment-with-locales.min.js"></script>
+		<script src="/my/js/bootstrap-datetimepicker.js"></script>
 	</head>
 	<body>
 		<div class="container">
 			<nav class="navbar navbar-expand-lg navbar-dark bg-dark">
-			  <a class="navbar-brand" href="">Admin</a>
-			  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-				<span class="navbar-toggler-icon"></span>
-			  </button>
-			  <div class="collapse navbar-collapse" id="navbarSupportedContent">
-				<ul class="navbar-nav mr-auto">
-					<li class="nav-item">
-						<a class="nav-link" href="/~sale25/a_gubun">구분</a>
-					</li>
-					<li class="nav-item">
-						<a class="nav-link" href="/~sale25/a_brand">브랜드</a>
-					</li>
-					<li class="nav-item">
-						<a class="nav-link" href="/~sale25/a_product">제품</a>
-					</li>
-					<li class="nav-item">
-						<a class="nav-link" href="/~sale25/a_member">사용자</a>
-					</li>
-				</ul>
-				<a href='/~sale25/l_login/logout' class='btn btn-sm btn-outline-success'>로그아웃</a>
-			  </div>
+			  	<a class="navbar-brand" href="">Admin</a>
+			  	<button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+					<span class="navbar-toggler-icon"></span>
+			  	</button>
+			  	<div class="collapse navbar-collapse" id="navbarSupportedContent">
+					<ul class="navbar-nav mr-auto">
+						<li class="nav-item">
+							<a class="nav-link" href="/a_gubun">구분</a>
+						</li>
+						<li class="nav-item">
+							<a class="nav-link" href="/a_brand">브랜드</a>
+						</li>
+						<li class="nav-item">
+							<a class="nav-link" href="/a_product">제품</a>
+						</li>
+						<li class="nav-item">
+							<a class="nav-link" href="/a_member">사용자</a>
+						</li>
+					</ul>
+					<a href='/l_login/logout' class='btn btn-sm btn-outline-success'>로그아웃</a>
+				  </div>
 			</nav>
 	
