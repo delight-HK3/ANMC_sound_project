75c279c52bd64afe30dbd6595a6d955653c048e5
leebean <74585673+delight-HK3@users.noreply.github.com>
2022-02-24 11:34:53 +0900

    Update a_main_header_nomenu.php
    css, script의 링크를 수정 했습니다.

--- view/a_main_header_nomenu.php
+++ view/a_main_header_nomenu.php
@@ -1,22 +1,19 @@
 <!DOCTYPE html>
 <html lang="kr">
 	<head>
-	<meta charset="utf-8">
-	<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-	<title>개별프로젝트 관리자 페이지</title>
-		<link href="/~sale25/my/css/bootstrap.css" rel="stylesheet">
-		<link href="/~sale25/my/css/my.css" rel="stylesheet">
-		<link href="/~sale25/my/css/bootstrap-datetimepicker.css" rel="stylesheet">
-		<link href="/~sale25/my/css/fontawesome-all.min.css" rel="stylesheet">
+		<meta charset="utf-8">
+		<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+		<title>개별프로젝트 관리자 페이지</title>
+		<link href="/my/css/bootstrap.css" rel="stylesheet">
+		<link href="/my/css/my.css" rel="stylesheet">
+		<link href="/my/css/bootstrap-datetimepicker.css" rel="stylesheet">
+		<link href="/my/css/fontawesome-all.min.css" rel="stylesheet">
 
-		<script src="/~sale25/my/js/jquery-3.5.1.min.js"></script>
-		<script src="/~sale25/my/js/popper.min.js"></script>
-		<script src="/~sale25/my/js/bootstrap.js"></script>
-		<script src="/~sale25/my/js/moment-with-locales.min.js"></script>
-		<script src="/~sale25/my/js/bootstrap-datetimepicker.js"></script>
+		<script src="/my/js/jquery-3.5.1.min.js"></script>
+		<script src="/my/js/popper.min.js"></script>
+		<script src="/my/js/bootstrap.js"></script>
+		<script src="/my/js/moment-with-locales.min.js"></script>
+		<script src="/my/js/bootstrap-datetimepicker.js"></script>
 	</head>
 	<body>
 		<div class="container">
-			
-
-	
