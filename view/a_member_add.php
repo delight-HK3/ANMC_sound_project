0ce24da8643f1f0e456fa5e50258d5faf1b98401
leebean <74585673+delight-HK3@users.noreply.github.com>
2022-02-24 11:37:50 +0900

    Update a_member_add.php
    코드 정리 작업을 했습니다.

--- view/a_member_add.php
+++ view/a_member_add.php
@@ -1,30 +1,30 @@
-	<div class="alert productcolor1 mymargin5" role="alert">사용자</div>
-		<form name="form1" method="post" action="">
-			<table class="table table-bordered table-sm mymargin5">
+<div class="alert productcolor1 mymargin5" role="alert">사용자</div>
+	<form name="form1" method="post" action="">
+		<table class="table table-bordered table-sm mymargin5">
 			<tr>
 				<td width="20%" class="productcolor1" style="vertical-align:middle">
 					<font color="red">*</font> 이름
 				</td>
 				<td width="80%" align="left">
 					<div class="form-inline">
-						<input  type="text" name="name" size="20" maxlength="20" value="<?=set_value("name") ?>"  class="form-control form-control-sm">
+						<input  type="text" name="name" size="20" maxlength="20" value="<?php echo set_value("name") ?>"  class="form-control form-control-sm">
 					</div>
-					<? 
+					<?php 
 						If (form_error("name")==true){ 
 							echo form_error("name"); 
 						}
 					?>
 				</td>
 			</tr>
-			 <tr>
+			<tr>
 				<td width="20%" class="productcolor1" style="vertical-align:middle">
 					<font color="red">*</font> 아이디
 				</td>
 				<td width="80%" align="left">
 					<div class="form-inline">
-						<input  type="text" name="uid" size="20" maxlength="20" value="<?=set_value("uid") ?>" class="form-control form-control-sm">
+						<input  type="text" name="uid" size="20" maxlength="20" value="<?php echo set_value("uid") ?>" class="form-control form-control-sm">
 					</div>
-					<? 
+					<?php 
 						If (form_error("uid")==true){ 
 							echo form_error("uid"); 
 						}
@@ -37,9 +37,9 @@
 				</td>
 				<td width="80%" align="left">
 					<div class="form-inline">
-						<input  type="password" name="pwd" size="20" maxlength="20" value="<?=set_value("pwd") ?>" class="form-control form-control-sm">
+						<input  type="password" name="pwd" size="20" maxlength="20" value="<?php echo set_value("pwd") ?>" class="form-control form-control-sm">
 					</div>
-					<? 
+					<?php
 						If (form_error("pwd")==true){ 
 							echo form_error("pwd"); 
 						}
@@ -68,10 +68,10 @@
 					</div>
 				</td>
 			</tr>
-			</table>
-			<div align="center">
-				<input type="submit" value="저장" class="btn btn-sm productcolor1">&nbsp
-				<input type="button" value="이전버튼" class="btn btn-sm productcolor1" onclick="history.back()">
-			</div>
-		</form>
-   
+		</table>
+		<div align="center">
+			<input type="submit" value="저장" class="btn btn-sm productcolor1">&nbsp
+			<input type="button" value="이전버튼" class="btn btn-sm productcolor1" onclick="history.back()">
+		</div>
+	</form>
+ 
