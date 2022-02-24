fc3994596c975d44bc9c8382bdebb0190e4ab814
leebean <74585673+delight-HK3@users.noreply.github.com>
2022-02-24 11:41:05 +0900

    Update a_member_edit.php
    코드 정리 작업을 했습니다.

--- view/a_member_edit.php
+++ view/a_member_edit.php
@@ -1,9 +1,9 @@
-	<div class="alert productcolor1 mymargin5" role="alert">사용자</div>
-		<form name="form1" method="post" action="">
-			<table class="table table-bordered table-sm mymargin5">
+<div class="alert productcolor1 mymargin5" role="alert">사용자</div>
+	<form name="form1" method="post" action="">
+		<table class="table table-bordered table-sm mymargin5">
 			<tr>
 				<td width="20%" class="productcolor1" style="vertical-align:middle">번호</td>
-				<td width="80%" align="left"><?=$row->no25 ?></td>
+				<td width="80%" align="left"><?php echo $row->no25 ?></td>
 			</tr>
 			<tr>
 				<td width="20%" class="productcolor1" style="vertical-align:middle">
@@ -11,9 +11,9 @@
 				</td>
 				<td width="80%" align="left">
 					<div class="form-inline">
-						<input  type="text" name="name" size="20" maxlength="20" value="<?=$row->name25 ?>"  class="form-control form-control-sm">
+						<input  type="text" name="name" size="20" maxlength="20" value="<?php echo $row->name25 ?>"  class="form-control form-control-sm">
 					</div>
-					<? 
+					<?php
 						If (form_error("name")==true){ 
 							echo form_error("name"); 
 						}
@@ -26,9 +26,9 @@
 				</td>
 				<td width="80%" align="left">
 					<div class="form-inline">
-						<input  type="text" name="uid" size="20" maxlength="20" value="<?=$row->uid25 ?>" class="form-control form-control-sm">
+						<input  type="text" name="uid" size="20" maxlength="20" value="<?php echo $row->uid25 ?>" class="form-control form-control-sm">
 					</div>
-					<? 
+					<?php 
 						If (form_error("uid")==true){ 
 							echo form_error("uid"); 
 						}
@@ -41,16 +41,16 @@
 				</td>
 				<td width="80%" align="left">
 					<div class="form-inline">
-						<input  type="password" name="pwd" size="20" maxlength="20" value="<?=$row->pwd25 ?>" class="form-control form-control-sm">
+						<input  type="password" name="pwd" size="20" maxlength="20" value="<?php echo $row->pwd25 ?>" class="form-control form-control-sm">
 					</div>
-					<? 
+					<?php 
 						If (form_error("pwd")==true){ 
 							echo form_error("pwd"); 
 						}
 					?>
 				</td>
 			</tr>
-			<?
+			<?php
 				$phone1 = trim(substr($row->phone25,0,3));      // 전화 : 지역번호 추출
 				$phone2 = trim(substr($row->phone25,3,4));      // 전화 : 국번호 추출
 				$phone3 = trim(substr($row->phone25,7,4));      // 전화 : 번호 추출
@@ -61,9 +61,9 @@
 				</td>
 				<td width="80%" align="left">
 					<div class="form-inline">
-						<input  type="text" name="phone1" size="3" maxlength="3" value="<?=$phone1?>" class="form-control form-control-sm">-
-						<input  type="text" name="phone2" size="4" maxlength="4" value="<?=$phone2?>" class="form-control form-control-sm">-
-						<input  type="text" name="phone3" size="4" maxlength="4" value="<?=$phone3?>" class="form-control form-control-sm">
+						<input  type="text" name="phone1" size="3" maxlength="3" value="<?php echo $phone1?>" class="form-control form-control-sm">-
+						<input  type="text" name="phone2" size="4" maxlength="4" value="<?php echo $phone2?>" class="form-control form-control-sm">-
+						<input  type="text" name="phone3" size="4" maxlength="4" value="<?php echo $phone3?>" class="form-control form-control-sm">
 					</div>
 				</td>
 			</tr>
@@ -71,7 +71,7 @@
 				<td width="20%" class="productcolor1" style="vertical-align:middle">등급</td>
 				<td width="80%" align="left">
 					<div class="form-inline">
-						<?
+						<?php
 							if ($row->rank25==1){
 								echo("<input type='radio' name='rank' value='1' checked>&nbsp관리자&nbsp&nbsp");
 								echo("<input type='radio' name='rank' value='2' >&nbsp직원&nbsp&nbsp");
@@ -91,10 +91,10 @@
 					</div>
 				</td>
 			</tr>
-			</table>
-			<div align="center">
-				<input type="submit" value="저장" class="btn btn-sm productcolor1">&nbsp
-				<input type="button" value="이전버튼" class="btn btn-sm productcolor1" onclick="history.back()">
-			</div>
-		</form>
+		</table>
+		<div align="center">
+			<input type="submit" value="저장" class="btn btn-sm productcolor1">&nbsp
+			<input type="button" value="이전버튼" class="btn btn-sm productcolor1" onclick="history.back()">
+		</div>
+	</form>
    
