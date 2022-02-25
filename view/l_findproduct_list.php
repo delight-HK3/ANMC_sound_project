9af46c6b591291c6170ed5931d3a674d8ecee9da
leebean <74585673+delight-HK3@users.noreply.github.com>
2022-02-25 14:02:14 +0900

    Update l_findproduct_list.php
    img, a 태그의 링크를 수정했고 코드 정리 작업을 했습니다.

--- view/l_findproduct_list.php
+++ view/l_findproduct_list.php
@@ -1,53 +1,58 @@
 <div style="background-color:#e6e6e6">
 	<hr style="border:solid 10px #e6e6e6;">
-<?
-	if(!$list)
-	{
-?>	
-		<div style="background-color:white;" class="container">
-			<center>
-				<br><br><br><img src='/~sale25/my/img/noproduct.png'>
-			</center>
-			<center>
-			<br><h3><b>"<?=$text1?>"</b>으로 검색한 상품이 없습니다.</h3><br>
-			</center>
-		</div>
-<?
-	}
-	else
-	{
-?>		
-		<div style="background-color:white" class="container" ><br>
-		<center><h4><b>"<?=$text1?>"</b>으로 검색한 결과 <b>"<?=$total?>"</b>개의 제품이 존재합니다.</h4></center>
-		<div class="row" style="margin-left:1px">
+		<?php
+			if(!$list)
+			{
+		?>	
+				<div style="background-color:white;" class="container">
+					<center>
+						<br><br><br><img src='/my/img/noproduct.png'>
+					</center>
+					<center>
+						<br><h3><b>"<?php echo $text1?>"</b>으로 검색한 상품이 없습니다.</h3><br>
+					</center>
+				</div>
+		<?php
+			}
+			else
+			{
+		?>		
+				<div style="background-color:white" class="container"><br>
+					<center>
+						<h4><b>"<?php echo $text1?>"</b>으로 검색한 결과 <b>"<?php echo $total?>"</b>개의 제품이 존재합니다.</h4>
+					</center>
+					<div class="row" style="margin-left:1px">
 		
-<?
-		foreach($list as $row)
-		{
-			$iname1=$row->pic25 ? $row->pic25 : "";
-			$iname2=$row->changepic25 ? $row->changepic25 : "";
-?>
-				<div class="card text-center shadow bg-body rounded" style="width: 16rem; margin:10px;">
-				  <center><div class="box">
-						<img src="/~sale25/product_img/<?=$iname2 ?>">
-						<img src="/~sale25/product_img/<?=$iname1 ?>">
-					</div></center>
-					<h5 class="card-title"><a href="/~sale25/l_gubunproduct/view/no/<?=$row->no25?>/page/<?=$page?>"><?=$row->name25 ?></a></h5>
-				  <div class="card-footer text-left" style="background-color: white">
-					<font style="font-size:15px">분류 : <b><?=$row->gubun_name25; ?></b></font><br>
-					<font style="font-size:15px">제조사 : <b><?=$row->brand_name25; ?></b></font><br>
-					<td align='right'>￦<?=number_format($row->price25) ?></td>
-				  </div>
-				</div>		
-<?
-		}
-?>
-			</div>
-			<br><?=$pagination;?><br>
-		</div>
-		
-<?
-	}
-?>
+			<?php
+				foreach($list as $row)
+				{
+					$iname1=$row->pic25 ? $row->pic25 : "";
+					$iname2=$row->changepic25 ? $row->changepic25 : "";
+			?>
+					<div class="card text-center shadow bg-body rounded" style="width: 16rem; margin:10px;">
+						<center>
+							<div class="box">
+								<img src="/product_img/<?php echo $iname2 ?>">
+								<img src="/product_img/<?php echo $iname1 ?>">
+							</div>
+						</center>
+						<h5 class="card-title">
+							<a href="/l_gubunproduct/view/no/<?php echo $row->no25?>/page/<?php echo $page?>"><?php echo $row->name25 ?></a>
+						</h5>
+						<div class="card-footer text-left" style="background-color: white">
+							<font style="font-size:15px">분류 : <b><?php echo $row->gubun_name25; ?></b></font><br>
+							<font style="font-size:15px">제조사 : <b><?php echo $row->brand_name25; ?></b></font><br>
+							<td align='right'>￦<?php echo number_format($row->price25) ?></td>
+						</div>
+					</div>		
+			<?php
+				}
+			?>
+					</div>
+					<br><?php echo $pagination;?><br>
+				</div>	
+		<?php
+			}
+		?>
 	<hr style="border:solid 10px #e6e6e6;">
 </div>
