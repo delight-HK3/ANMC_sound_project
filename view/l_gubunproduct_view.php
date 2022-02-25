d633c3d3c9ff6d8869f1c5b0f2fe4527712078ab
leebean <74585673+delight-HK3@users.noreply.github.com>
2022-02-25 14:07:22 +0900

    Update l_gubunproduct_view.php
    img태그의 링크를 수정했습니다.

--- view/l_gubunproduct_view.php
+++ view/l_gubunproduct_view.php
@@ -1,6 +1,3 @@
-<script>
-	
-</script>
 <div style="background-color:#e6e6e6">
 	<hr style="border:solid 10px #e6e6e6;">
 		<div style="background-color:white" class="container"><br>
@@ -9,24 +6,24 @@
 					<center>
 						<div style="height:555px; width:430px;">	
 							<div class="viewbox">
-								<img src="/~sale25/product_img/<?=$row->changepic25?>">
-								<img src="/~sale25/product_img/<?=$row->pic25?>">
+								<img src="/product_img/<?php echo $row->changepic25?>">
+								<img src="/product_img/<?php echo $row->pic25?>">
 							</div>
 							<br>
 							<div class="miniimg" style="float:left" >
-								<img src="/~sale25/product_img/<?=$row->pic25?>">&nbsp&nbsp
-								<img src="/~sale25/product_img/<?=$row->changepic25?>">	
+								<img src="/product_img/<?php echo $row->pic25?>">&nbsp&nbsp
+								<img src="/product_img/<?php echo $row->changepic25?>">	
 							</div>
 						</div>
 					</center>
 					<br>
 				</div>
 				<div class="card" style="width: 30rem; margin:10px; height:355px; border:1px; border-color: lightgray; border-style: solid; border-width: 1px; ">
-					<h4 class="card-header bg-primary" ><font style="color:white"><?=$row->name25?></font></h4>
+					<h4 class="card-header bg-primary" ><font style="color:white"><?php echo $row->name25?></font></h4>
 					<div class="card-body">
-						<p style="text-align:left; margin-left:20px; font-size:20px;"><b>구분명 :</b> <?=$row->gubun_name25?></p>
-						<p style="text-align:left; margin-left:20px; font-size:20px;"><b>브랜드명 :</b> <?=$row->brand_name25?></p>
-						<p style="text-align:left; margin-left:20px; font-size:20px;"><b>가격 :</b> <?=number_format($row->price25)?></p>
+						<p style="text-align:left; margin-left:20px; font-size:20px;"><b>구분명 :</b> <?php echo $row->gubun_name25?></p>
+						<p style="text-align:left; margin-left:20px; font-size:20px;"><b>브랜드명 :</b> <?php echo $row->brand_name25?></p>
+						<p style="text-align:left; margin-left:20px; font-size:20px;"><b>가격 :</b> <?php echo number_format($row->price25)?></p>
 					</div>
 					<div class="card-footer">
 						<a href="" type="button" class="btn btn-block btn-success" >제품구매</a>
@@ -38,10 +35,9 @@
 			<center>
 				<br><br><br>
 				<h4 style="text-align:center">상품상세정보</h4><hr><br>
-				<img src="/~sale25/product_img/<?=$row->detailpic25?>">
+				<img src="/product_img/<?php echo $row->detailpic25?>">
 				<br><br><br><br><br>
 			</center>
 		</div>
-		
 	<hr style="border:solid 10px #e6e6e6;">
 </div>
