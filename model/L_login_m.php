17e29d0f46a51259570b197d10df9d33f16c4a8d
leebean <74585673+delight-HK3@users.noreply.github.com>
2022-04-18 09:23:45 +0900

    Update L_login_m.php

--- model/L_login_m.php
+++ model/L_login_m.php
@@ -1,10 +1,12 @@
 <?php
-    class L_login_m extends CI_Model     // 모델 클래스 선언
-    {
+if (!defined('BASEPATH')) exit('No direct script access allowed');
+
+class L_login_m extends CI_Model     // 모델 클래스 선언
+{
         public function getrow($uid,$pwd)//정보목록보기
         {
 		$sql="select * from promember where uid25='$uid' and pwd25='$pwd'";
 		return $this->db->query($sql)->row();
-        }
-    }
+	}
+}
 ?>
